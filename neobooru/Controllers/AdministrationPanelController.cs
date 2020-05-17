4ba73eb27079d6a628f27a83f3538950a10152b4
Kornel Domeradzki <korneldomeradzki1@gmail.com>
2020-05-17 17:49:37 +0200

    Added a missing js dependency and added custom role creation

--- neobooru/Controllers/AdministrationPanelController.cs
+++ neobooru/Controllers/AdministrationPanelController.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using neobooru.ViewModels.Forms;
 
 namespace neobooru.Controllers
 {
     // remember that u can override this with [AllowAnonymous]
-    [Authorize]
+    // [Authorize]
     public class AdministrationPanelController : Controller
     {
         
-        private readonly string[] _subsectionPages = { "Main Panel", "List Roles", "Help" };
+        private readonly string[] _subsectionPages = { "Main Panel", "List Users", "List Roles", "Create Role", "Help" };
+        private readonly RoleManager<IdentityRole> _roleManager;
 
+        public AdministrationPanelController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
 
         [HttpGet]
         public IActionResult MainPanel()
@@ -24,18 +32,10 @@ namespace neobooru.Controllers
         }
 
         [HttpGet]
-        public IActionResult ListRoles()
+        public IActionResult ListUsers()
         {
             ViewBag.SubsectionPages = _subsectionPages;
             ViewBag.ActiveSubpage = _subsectionPages[1];
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult Help()
-        {
-            ViewBag.SubsectionPages = _subsectionPages;
-            ViewBag.ActiveSubpage = _subsectionPages[2];
             return View();
         }
 
@@ -48,10 +48,55 @@ namespace neobooru.Controllers
         }
 
         [HttpGet]
+        public IActionResult ListRoles()
+        {
+            ViewBag.SubsectionPages = _subsectionPages;
+            ViewBag.ActiveSubpage = _subsectionPages[2];
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            ViewBag.SubsectionPages = _subsectionPages;
+            ViewBag.ActiveSubpage = _subsectionPages[3];
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel createRoleViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole 
+                {
+                    Name = createRoleViewModel.RoleName
+                };
+
+                IdentityResult result = await _roleManager.CreateAsync(identityRole);
+                if (result.Succeeded)
+                    return RedirectToAction("MainPanel", "AdministrationPanel");
+                foreach (IdentityError error in result.Errors)
+                    ModelState.AddModelError("", error.Description);
+            }
+
+
+            return View(createRoleViewModel);
+        }
+
+        [HttpGet]
         public IActionResult EditRole()
         {
             ViewBag.SubsectionPages = _subsectionPages;
             ViewBag.ActiveSubpage = "Edit Role";
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Help()
+        {
+            ViewBag.SubsectionPages = _subsectionPages;
+            ViewBag.ActiveSubpage = _subsectionPages[4];
             return View();
         }
     }
